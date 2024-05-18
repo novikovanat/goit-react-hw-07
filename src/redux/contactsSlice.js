49bfbca8e32ff81c3398a2f18f43d54cca8556a1
novikovanat <72397136+novikovanat@users.noreply.github.com>
2024-05-18 22:25:51 +0200

    exR added

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,28 +1,50 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { nanoid } from "nanoid";
+import {fetchContacts} from "./contactsOps";
 // import testData from "../components/testData.json";
+
+const handlePending = (state) => {
+  state.isLoading = true;
+};
+
+const handleRejected = (state, action) => {
+  state.isLoading = false;
+  state.error = action.payload;
+ 
+};
+
+const handleSuccess = (state, action) => {
+  state.isLoading = false;
+  state.error = null;
+  state.items = action.payload;
+};
 
 const contactsSlice = createSlice({
   name: "contacts",
   initialState: { item: [], loading: false, error: null },
-  reducers: {
-    addContact: {
-      reducer(state, action) {
-        state.item.push(action.payload);
-      },
-      prepare(contact) {
-        return {
-          payload: { id: nanoid(6), ...contact },
-        };
-      },
-    },
-    deleteContact: {
-      reducer(state, action) {
-        const contacts = state.item;
-        const index = contacts.findIndex((task) => task.id === action.payload);
-        contacts.splice(index, 1);
-      },
-    },
+  // reducers: {
+  //   addContact: {
+  //     reducer(state, action) {
+  //       state.item.push(action.payload);
+  //     },
+  //     prepare(contact) {
+  //       return {
+  //         payload: { id: nanoid(6), ...contact },
+  //       };
+  //     },
+  //   },
+  //   deleteContact: {
+  //     reducer(state, action) {
+  //       const contacts = state.item;
+  //       const index = contacts.findIndex((task) => task.id === action.payload);
+  //       contacts.splice(index, 1);
+  //     },
+  //   },
+  // },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchContacts.pending, handlePending)
+      .addCase(fetchContacts.fulfilled, handleRejected)
+      .addCase(fetchContacts.rejected, handleSuccess);
   },
 });
 
