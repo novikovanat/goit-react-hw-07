4a8c1022aa8b8bf50184b3ff84d67f03fda688d9
novikovanat <72397136+novikovanat@users.noreply.github.com>
2024-05-18 22:30:49 +0200

    fetch changed

--- src/redux/contactsOps.js
+++ src/redux/contactsOps.js
@@ -3,7 +3,14 @@ import axios from "axios";
 
 axios.defaults.baseURL = "https://6648f1364032b1331becde79.mockapi.io/";
 
-export const fetchContacts = createAsyncThunk("contacts/fetchAll", async () => {
-  const response = await axios.get("/contacts");
-  return response.data;
-});
+export const fetchContacts = createAsyncThunk(
+  "contacts/fetchAll",
+  async (_, thunkAPI) => {
+    try {
+      const response = await axios.get("/contacts");
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
