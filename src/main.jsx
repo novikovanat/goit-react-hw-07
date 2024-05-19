83251b578aab61aeebdda7d0d6f1cb5c5a1d1908
novikovanat <72397136+novikovanat@users.noreply.github.com>
2024-05-19 16:47:45 +0200

    presistor removed

--- src/main.jsx
+++ src/main.jsx
@@ -3,17 +3,13 @@ import ReactDOM from "react-dom/client";
 import "modern-normalize";
 import App from "./components/App/App";
 import "./index.css";
-import { persistor } from "./redux/store";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
-import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading="wait... for it" persistor={persistor}>
-        <App />
-      </PersistGate>
+      <App />
     </Provider>
   </React.StrictMode>
 );
